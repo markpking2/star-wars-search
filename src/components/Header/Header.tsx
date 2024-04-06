87550e18fe79adddc1df312158677f812b32fa75
markpking2 <markpking2@gmail.com>
2024-04-06 18:38:27 -0400

    make header use default colors for color modes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,9 @@
-import { Box, Flex, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { SearchBar } from "@/components/SearchBar";
 
 export const Header = () => {
-  const bgColor = useColorModeValue("gray.50", "gray.800");
-  const textColor = useColorModeValue("black", "white");
-
   return (
-    <Box bg={bgColor} color={textColor} w="100%" p={4}>
+    <Box w="100%" p={4}>
       <Flex direction="column" align="center" justify="center">
         <Heading as="h1" size="2xl" mb={4}>
           Star Wars Character Search
