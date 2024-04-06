66d32a62adef6018d33376d89882b8a9bd946dd7
markpking2 <markpking2@gmail.com>
2024-04-06 11:59:07 -0400

    add some colors to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,17 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export const theme = extendTheme({});
+export const theme = extendTheme({
+  colors: {
+    starWars: {
+      lightGray: "#E0E0E0",
+      gray: "#C9C9C9",
+      darkGray: "#A1A1A1",
+      veryDarkGray: "#727272",
+      veryVeryDarkGray: "#4A4A4A",
+      black: "#262626",
+      red: "#FF0000",
+      green: "#00FF00",
+      blue: "#0000FF",
+    },
+  },
+});
